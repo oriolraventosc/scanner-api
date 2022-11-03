c09dad7bd06d3cf59c0f3a6f45f095d8eda24120
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-03 18:42:02 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/no-empty-function": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: /^_/.source,
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: /^_$/.source,
+      },
+    ],
+  },
+};
