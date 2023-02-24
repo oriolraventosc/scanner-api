e3128d0312cea4cdd9eb6681642104b5aaaa239e
oriolraventosc <oriol.raventos.c@gmail.com>
2023-02-24 09:44:56 +0100

    Create Product model

---
+++ src/database/models/product/product.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema({
+  name: String,
+  image: String,
+  price: Number,
+  ingredients: String,
+  brand: String,
+  weight: Number,
+  benefits: [String],
+});
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Product = model("Product", productSchema, "Products");
+
+export default Product;
