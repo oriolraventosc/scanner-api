c90e434b02da24061701020088c528b21828718d
oriolraventosc <oriol.raventos.c@gmail.com>
2023-05-17 06:19:05 +0200

    Update user structure

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,7 @@ export interface UserStructure {
   email: string;
   password: string;
   name: string;
+  favouriteProducts: ProductStructure[];
 }
 
 export interface UserTokenPayload extends JwtPayload {
